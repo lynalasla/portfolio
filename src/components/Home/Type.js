f246750ff4e19edc5ede3d15b97b654d63d803e9
Lyna Lasla <104279881+lynalasla@users.noreply.github.com>
2024-11-06 18:34:53 +0100

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -3,7 +3,7 @@ import Typewriter from "typewriter-effect";
 
 function Type() {
   return (
-   <div style={{ marginTop: '-30px' }}> {/* Ajustez la valeur selon vos besoins */}
+   <div style={{ marginTop: '-20px' }}> {/* Ajustez la valeur selon vos besoins */}
     <Typewriter
       options={{
         strings: [
