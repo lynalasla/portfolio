60d29dd852d95f6d71e6d721d93f40933f8ace46
Lyna Lasla <104279881+lynalasla@users.noreply.github.com>
2024-11-30 20:21:25 +0100

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,49 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  SiJupyter, // Jupyter Notebook
-  SiAnaconda, // Anaconda
-  SiTensorflow, // TensorFlow
-  SiPytorch, // PyTorch
-  SiGooglecolab, // Google Colab
-  SiVisualstudiocode, // VS Code
-  SiPowerbi, // Power BI
-  SiTalend, // Talend
-  SiFigma, // Figma
+  SiJupyter,
+  SiAnaconda,
+  SiTensorflow,
+  SiPytorch,
+  SiGooglecolab,
+  SiVisualstudiocode,
+  SiPowerbi,
+  SiTalend,
+  SiFigma,
 } from "react-icons/si";
 
 function Toolstack() {
+  const [tooltip, setTooltip] = useState("");
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("Jupyter Notebook")}
+        onMouseLeave={() => setTooltip("")}
+      >
         <SiJupyter />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("Anaconda")}
+        onMouseLeave={() => setTooltip("")}
+      >
         <SiAnaconda />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("TensorFlow")}
+        onMouseLeave={() => setTooltip("")}
+      >
         <SiTensorflow />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("PyTorch")}
+        onMouseLeave={() => setTooltip("")}
+      >
         <SiPytorch />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("Google Colab")}
+        onMouseLeave={() => setTooltip("")}
+      >
         <SiGooglecolab />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("VS Code")}
+        onMouseLeave={() => setTooltip("")}
+      >
         <SiVisualstudiocode />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("Power BI")}
+        onMouseLeave={() => setTooltip("")}
+      >
         <SiPowerbi />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("Talend")}
+        onMouseLeave={() => setTooltip("")}
+      >
         <SiTalend />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFigma /> {/* Figma for UI/UX design */}
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("UI/UX Design - Figma")}
+        onMouseLeave={() => setTooltip("")}
+      >
+        <SiFigma />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
     </Row>
   );
 }
+
+const tooltipStyle = {
+  position: "absolute",
+  bottom: "-30px",
+  left: "50%",
+  transform: "translateX(-50%)",
+  backgroundColor: "#333",
+  color: "white",
+  padding: "5px 10px",
+  borderRadius: "5px",
+  fontSize: "12px",
+};
 
 export default Toolstack;
