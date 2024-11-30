161f25a5f5cfbeb899b79b9fbceb164b5e87a935
Lyna Lasla <104279881+lynalasla@users.noreply.github.com>
2024-11-30 20:21:03 +0100

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,49 +1,161 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import { DiJavascript1, DiReact, DiMongodb, DiPython, DiMysql } from "react-icons/di";
-import { SiPostgresql, SiMicrosoftexcel, SiPandas, SiNumpy, SiScipy, SiScikitlearn, SiFigma } from "react-icons/si";
+import {
+  DiJavascript1,
+  DiReact,
+  DiMongodb,
+  DiPython,
+  DiMysql,
+} from "react-icons/di";
+import {
+  SiPostgresql,
+  SiMicrosoftexcel,
+  SiPandas,
+  SiNumpy,
+  SiScipy,
+  SiScikitlearn,
+  SiFigma,
+} from "react-icons/si";
 
 function Techstack() {
+  const [tooltip, setTooltip] = useState("");
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiScikitlearn /> {/* KNN, Matrice de Factorisation */}
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("KNN, Matrice de Factorisation")}
+        onMouseLeave={() => setTooltip("")}
+      >
+        <SiScikitlearn />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNumpy /> {/* Réseaux Neurones */}
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("Réseaux Neurones")}
+        onMouseLeave={() => setTooltip("")}
+      >
+        <SiNumpy />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact /> {/* React.js */}
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("React.js")}
+        onMouseLeave={() => setTooltip("")}
+      >
+        <DiReact />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 /> {/* JavaScript */}
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("JavaScript")}
+        onMouseLeave={() => setTooltip("")}
+      >
+        <DiJavascript1 />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython /> {/* Traitement du Langage Naturel (NLP) */}
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("Traitement du Langage Naturel (NLP)")}
+        onMouseLeave={() => setTooltip("")}
+      >
+        <DiPython />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb /> {/* Base de données MongoDB */}
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("Base de données MongoDB")}
+        onMouseLeave={() => setTooltip("")}
+      >
+        <DiMongodb />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMysql /> {/* Base de données MySQL */}
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("Base de données MySQL")}
+        onMouseLeave={() => setTooltip("")}
+      >
+        <DiMysql />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql /> {/* Base de données PostgreSQL */}
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("Base de données PostgreSQL")}
+        onMouseLeave={() => setTooltip("")}
+      >
+        <SiPostgresql />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMicrosoftexcel /> {/* Excel pour les analyses de données */}
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("Excel pour les analyses de données")}
+        onMouseLeave={() => setTooltip("")}
+      >
+        <SiMicrosoftexcel />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPandas /> {/* Bibliothèque Pandas pour la manipulation de données */}
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("Bibliothèque Pandas")}
+        onMouseLeave={() => setTooltip("")}
+      >
+        <SiPandas />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiScipy /> {/* Bibliothèque SciPy pour les calculs scientifiques */}
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("Bibliothèque SciPy")}
+        onMouseLeave={() => setTooltip("")}
+      >
+        <SiScipy />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFigma /> {/* UI/UX Design */}
+      <Col
+        xs={4}
+        md={2}
+        className="tech-icons"
+        onMouseEnter={() => setTooltip("UI/UX Design")}
+        onMouseLeave={() => setTooltip("")}
+      >
+        <SiFigma />
+        {tooltip && <div style={tooltipStyle}>{tooltip}</div>}
       </Col>
     </Row>
   );
 }
+
+const tooltipStyle = {
+  position: "absolute",
+  bottom: "-30px",
+  left: "50%",
+  transform: "translateX(-50%)",
+  backgroundColor: "#333",
+  color: "white",
+  padding: "5px 10px",
+  borderRadius: "5px",
+  fontSize: "12px",
+};
 
 export default Techstack;
