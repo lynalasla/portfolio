318c2a0c76ac32d2d835cf7036e26952f11e9bbd
Lyna Lasla <104279881+lynalasla@users.noreply.github.com>
2026-04-22 13:57:13 +0200

    Refactor AboutCard.js for cleaner HTML structure
    Replaced multiple <br /> tags with a single <br/> tag and removed an unnecessary <b/> tag.

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,18 +9,17 @@ function AboutCard() {
         <blockquote className="blockquote mb-0" style={{ marginTop: "20px" }}>
           <p style={{ textAlign: "justify" }}>
             Salut, je m'appelle <span className="purple">Lasla Lyna</span> !
-            <br />
-            <br />
+            <br/>
+            <br/>
             Je suis actuellement étudiante en Mastère Data & IA à l’École Multimédia à Paris,
             avec une spécialisation en Data Science, Machine Learning, intelligence artificielle et développement web.
-            <br />
-            <br />
+            <br/>
+            <b/>
             En parallèle de mon parcours académique, j’ai développé une expérience concrète à travers plusieurs stages
             et une expérience en tant que Data Scientist, me permettant de renforcer mes compétences en analyse de données, 
             modélisation, développement web et mise en place de pipelines data. J’ai également complété ma formation par 
             des projets pratiques et des apprentissages complémentaires afin d’approfondir mes compétences techniques.
-
-            <br />
+            <br/>
           </p>
         </blockquote>
       </Card.Body>
